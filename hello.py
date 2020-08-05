7ea0156097f6bc53fe502508584c31167799f5e7
Sajidk25 <sajidk25@gmail.com>
2020-08-05 23:57:40 +0600

    k8 cluster implemented and RolloutUpdate strategy with readinessProbe observed

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/bye")
+def bye():
+    return "Goodbye World!"
